90d9ffbec4e7a1b4ced35d8ed0bc412154467040
Alfredo Gabriel <amgabriel1@hotmail.com>
2023-04-10 00:12:14 -0300

    feat: arduino project

---
+++ arduinoserver/src/main/java/facomp/ufpa/br/Application.java
@@ -0,0 +1,39 @@
+package facomp.ufpa.br;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Application {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+    }
+
+    public void stop() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Application server = new Application();
+        server.start(9500);
+    }
+}
